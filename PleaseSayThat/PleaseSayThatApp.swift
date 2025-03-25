272e3c284cffa11649ccf4ceca8e4283f5afae4a
chxhyxn <chxhyxn@gmail.com>
2025-03-25 14:56:16 +0900

    Initial Commit

---
+++ PleaseSayThat/PleaseSayThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseSayThatApp.swift
+//  PleaseSayThat
+//
+//  Created by SeanCho on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PleaseSayThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
