66c54857b1bdd9488c6606deb8c5eea0b581d4fa
chxhyxn <chxhyxn@gmail.com>
2025-03-26 23:06:11 +0900

    apply createroomview design

--- PleaseSayThat/Views/CreateRoomView.swift
+++ PleaseSayThat/Views/CreateRoomView.swift
@@ -9,104 +9,121 @@ struct CreateRoomView: View {
     @State private var alertMessage = ""
     
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
             // 헤더
-            HStack {
+            HStack{
                 Button(action: {
                     viewModel.navigateToMain()
                 }) {
-                    Text("← 이전으로")
-                        .font(.system(size: 13))
-                        .foregroundColor(.black)
-                        .padding()
+                    HStack(spacing: 2) {
+                        Image(systemName: "arrow.left")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                        
+                        Text("이전으로")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundColor(.black)
+                            .padding()
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .leading)
                 }
                 .buttonStyle(.plain)
 
                 Spacer()
             }
             
-            VStack {
+            VStack(spacing: 12) {
                 HStack {
-                    Text("미팅룸 이름")
-                        .font(.system(size: 13))
+                    Text("팀명")
+                        .font(.system(size: 14))
                         .foregroundColor(.black)
                     Spacer()
                 }
                 
-                TextField("방 이름을 입력하세요", text: $roomName)
+                TextField("팀명을 입력해주세요", text: $roomName)
                     .textFieldStyle(PlainTextFieldStyle()) // 기본 스타일 제거
-                    .padding(10)
-                    .background(
+                    .padding(16)
+                    .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .leading)
+                    .background(.white)
+                    .cornerRadius(8)
+                    .overlay(
                         RoundedRectangle(cornerRadius: 8)
-                            .fill(.white)
+                        .inset(by: -0.5)
+                        .stroke(.gray, lineWidth: 1)
                     )
-                    .overlay(content: {
-                        RoundedRectangle(cornerRadius: 8)
-                            .strokeBorder(Color.gray, lineWidth: 1)
-                    })
-                    .padding(.horizontal)
+                
+                Spacer()
+                    .frame(height: 12)
                 
                 HStack {
                     Text("인원 수 설정")
-                        .font(.system(size: 13))
+                        .font(.system(size: 14))
                         .foregroundColor(.black)
                     Spacer()
                 }
                 
-                HStack(spacing: 0) {
+                HStack(spacing: 8) {
                     Button(action: {
                         if maxMembers > 2 {
                             maxMembers -= 1
                         }
                     }) {
-                        Image(systemName: "chevron.down")
-                            .font(.system(size: 20))
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.white)
-                            .background(Color(red: 0.4784, green: 0.5451, blue: 0.2314))
-                            .cornerRadius(10)
+                        Image(systemName: "minus")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 16, height: 16)
+                            .padding(16)
+                            .background(.lightGray)
+                            .cornerRadius(8)
                     }
                     .buttonStyle(.plain)
                     
                     Text("\(maxMembers)")
-                        .background(
-                            RoundedRectangle(cornerRadius: 0)
-                                .fill(.white)
-                        )
-                        .overlay(content: {
-                            RoundedRectangle(cornerRadius: 0)
-                                .strokeBorder(Color.gray, lineWidth: 1)
-                        })
+                        .font(.system(size: 16))
+                        .padding(16)
+                        .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .center)
+                        .background(.white)
+                        .cornerRadius(8)
                     
                     Button(action: {
                         if maxMembers < 10 {
                             maxMembers += 1
                         }
                     }) {
-                        Image(systemName: "chevron.up")
-                            .font(.system(size: 20))
-                            .frame(width: 40, height: 40)
-                            .foregroundColor(.white)
-                            .background(Color(red: 0.4784, green: 0.5451, blue: 0.2314))
-                            .cornerRadius(10)
+                        Image(systemName: "plus")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 16, height: 16)
+                            .padding(16)
+                            .background(.lightGray)
+                            .cornerRadius(8)
                     }
                     .buttonStyle(.plain)
                 }
                 
+                Spacer()
+                    .frame(height: 12)
                 
                 Button {
                     createRoom()
                 } label: {
-                    Text("미팅룸 생성")
+                    Text("팀 생성")
                         .font(.system(size: 15))
-                        .foregroundColor(.white)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 12)
+                        .frame(maxWidth: .infinity, minHeight: 48, maxHeight: 48, alignment: .center)
+                        .foregroundStyle(.white)
                         .background(.accent)
+                        .cornerRadius(8)
                 }
-                .cornerRadius(10)
                 .buttonStyle(.plain)
                 .disabled(roomName.isEmpty)
-                .opacity(roomName.isEmpty ? 0.8 : 1.0)
+                .opacity(roomName.isEmpty ? 0.7 : 1.0)
             }
+            .frame(maxWidth: 368, maxHeight: 272)
             
             Spacer()
             
