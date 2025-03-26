50df828136587645af44effec11fcf5187557a62
chxhyxn <chxhyxn@gmail.com>
2025-03-27 07:42:37 +0900

    edit popover image

--- PleaseSayThat/App/StatusBarController.swift
+++ PleaseSayThat/App/StatusBarController.swift
@@ -17,13 +17,15 @@ class StatusBarController {
         popover = NSPopover()
         popover.contentSize = NSSize(width: 240, height: 180)
         popover.behavior = .applicationDefined
+//        popover.delegate = self
         
         // 그 다음 statusBar 초기화
         statusBar = NSStatusBar.system
         statusItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
         
         if let statusBarButton = statusItem.button {
-            statusBarButton.image = NSImage(systemSymbolName: "message", accessibilityDescription: "Message")
+            // 시작할 때는 popover가 닫혀있으므로 picket_off 이미지 사용
+            statusBarButton.image = NSImage(named: "picket_off")
             statusBarButton.action = #selector(togglePopover)
             statusBarButton.target = self
         }
@@ -34,11 +36,33 @@ class StatusBarController {
     
     @objc func togglePopover() {
         if popover.isShown {
-            popover.performClose(nil)
+            closePopover()
         } else {
-            if let statusBarButton = statusItem.button {
-                popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: .minY)
-            }
+            showPopover()
         }
     }
+    
+    func showPopover() {
+        if let statusBarButton = statusItem.button {
+            // popover가 보여질 때 picket_on 이미지로 변경
+            statusBarButton.image = NSImage(named: "picket_on")
+            popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: .minY)
+            
+            // 팝오버가 열렸을 때 focus를 가져오도록 설정
+            popover.contentViewController?.view.window?.makeKey()
+        }
+    }
+    
+    func closePopover() {
+        // popover가 닫힐 때 picket_off 이미지로 변경
+        statusItem.button?.image = NSImage(named: "picket_off")
+        popover.performClose(nil)
+    }
 }
+
+// NSPopoverDelegate를 추가하여 popover가 외부 클릭으로 닫힐 때도 이미지 변경
+//extension StatusBarController: NSPopoverDelegate {
+//    func popoverDidClose(_ notification: Notification) {
+//        statusItem.button?.image = NSImage(named: "picket_off")
+//    }
+//}
